5b9fac9149e6acb3a57982a20e597c46b79b4b83
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-09-10 18:55:54 -0300

    Init

---
+++ app/post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+}
